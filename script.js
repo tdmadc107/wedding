3b2c897f59c56cf97f200b7d70c17777dbae51c3
tdmadc107 <dangdat111@gmail.com>
2024-05-15 23:40:09 +0700

    Init

---
+++ script.js
@@ -0,0 +1,19 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Auto slide
+setInterval(nextSlide, 15000);
